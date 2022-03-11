06b95f8479f4bf6f9d29f3e151013cea8cfb489c
root <neopotwana@gmail.com>
2022-03-11 15:40:55 -0800

    indentation fixed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
-*main - Entry point
-*My hello. printf task
-*Retur;1 after program execution
-*/
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n");
-Return (0)
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
-
