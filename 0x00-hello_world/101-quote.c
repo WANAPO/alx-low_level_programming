9f2fdabda9edf76677aae9380cccba020feb04b9
root <neopotwana@gmail.com>
2022-03-13 16:24:40 -0700

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
-#include <unistd.h>
+#include <stdio.h>
 /*
  * indentation is proving to be difficult
  * another try
-i */
+ */
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
