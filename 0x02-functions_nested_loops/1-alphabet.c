b22cefe88ae62e7feef495d60e70ac50aadb383f
WANAPO <neopotwana@gmail.com>
2022-03-16 16:59:39 -0700

    a;phabet printer

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,17 @@
-#include <stdio.h>
+#include <main.h>
 /**
  *prints_alphabet fnc that prints alphabet in lowercase
  *
- * Return: Always 0 (Success)
  */
-
-void print_alphabet(void)
+void print-alphabet(void)
 {
-    printf("abcdefghijklmnopqrstuvwxyz\n");
+	char c;
+
+	for (c = 'a';c <- 'z';c++)
+	{
+	_putchar (c);
+	}
+	_putchar('\n');
 }
+
+	
