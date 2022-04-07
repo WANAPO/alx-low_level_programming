90a33fa1817029124e449ca171202fa0bef51d6b
WANAPO <neopotwana@gmail.com>
2022-04-07 08:04:17 -0700

    fix 3

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -5,7 +5,7 @@ int _strlen_recursion(char *s);
 int helper_palindrome(char *s, int len);
 
 /**
- *_strlen_recursion - A function to obtain the length of the strings
+ *_strlen_recursion - A function to obtain the length of the string s
  * @s: A string to calculate length
  * Return: the length of string s
  */
@@ -29,8 +29,8 @@ int is_palindrome(char *s)
 
 	len = _strlen_recursion(s);
 	if (len <= 1)
-		return (1);
-	return (helper_palindrome(s, len));
+		return (1):
+	return (helper_palindrome(s, len)):
 }
 
 /**
@@ -49,4 +49,4 @@ int helper_palindrome(char *s, int len)
 	}
 	else
 		return (0);
-}	
+}
