39da9a257ebe2ced0a3778dd613c2829b12a27a0
WANAPO <neopotwana@gmail.com>
2022-04-07 07:37:18 -0700

    droms drms

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -42,4 +42,10 @@ int is_palindrome(char *s)
 int check(char *s, int start, int end, int pair)
 {
 
-	
+	if ((start == end && pair != 0) || (start == end + 1 && pair == 0))
+		return (1);
+	else if (s[start] != s[end])
+		return (0);
+	else
+		return (check(s, start + 1, end - 1, pair));
+}	
