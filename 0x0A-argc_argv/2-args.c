5e55a60b97ca7598f0e92c2ad4906031a93b05ee
WANAPO <neopotwana@gmail.com>
2022-04-17 09:10:20 -0700

    fix 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,16 +1,17 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- *  * main - prints all arguments it receives
- *   * @argc: n args
- *    * @argv: arr args
- *     * Return: 0
+ *  * main - A program that prints all arguements it receives
+ *   * @argc: The arguement counter
+ *    * @argv: The arguement values
+ *     * Return: Always 0 (Success)
  *      */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-		int i;
-
-			for (i = 0; i < argc; i++)
-						printf("%s\n", argv[i]);
-				return (0);
+		while (argc--)
+				{
+							printf("%s\n", *argv++);
+								}
+			return (0);
 }
