481e07ab0686cb9da7bce72d464c87ea2f8ba6ca
WANAPO <neopotwana@gmail.com>
2022-06-08 18:05:44 -0700

    fix 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,12 +1,12 @@
-#ifndef FILE_CALC
-#define FILE_CALC
+#ifndef CALC_H
+#define CALC_H
 
 /**
- * struct op - Structure op
- * @op: operator
- * @f: function
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
  */
-
 typedef struct op
 {
 	char *op;
@@ -14,15 +14,9 @@ typedef struct op
 } op_t;
 
 int op_add(int a, int b);
-
 int op_sub(int a, int b);
-
 int op_mul(int a, int b);
-
 int op_div(int a, int b);
-
 int op_mod(int a, int b);
-
 int (*get_op_func(char *s))(int, int);
-
 #endif
